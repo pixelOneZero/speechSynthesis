d776cdeb36ab2643f4adbd3c4d9e6fffec2047f1
Michael McCarthy <mike@pixelonezero.com>
2021-12-18 18:49:50 -0800

    Changes: * Assign SpeechSynthesisUtterance code in a named function * Invoke named function when user clicks button * Wrap JavaScript in speech.js within an immediately invoked function

--- js/speech.js
+++ js/speech.js
@@ -1,20 +1,25 @@
-var msg = new SpeechSynthesisUtterance();
-var voices = window.speechSynthesis.getVoices();
-msg.voice = voices[10]; // Note: some voices don't support altering params
-msg.voiceURI = 'native';
-msg.volume = 1; // 0 to 1
-msg.rate = 1; // 0.1 to 10
-msg.pitch = 1; //0 to 2
-msg.text = 'This is the speech synthesis API. The length of voices is ' + voices.length + ".  The rate of speech is " + msg.rate;
-msg.lang = 'en-US';
+(() => {
 
-msg.onend = function(e) {
-  console.log('Finished in ' + event.elapsedTime + ' seconds.');
-};
+  window.initialize = () => {
+    const msg = new SpeechSynthesisUtterance();
+    var voices = window.speechSynthesis.getVoices();
+    msg.voice = voices[0]; // Note: some voices don't support altering params
+    msg.voiceURI = 'native';
+    msg.volume = 1; // 0 to 1
+    msg.rate = 1; // 0.1 to 10
+    msg.pitch = 1; //0 to 2
+    msg.text = 'This is the speech synthesis API. The length of voices is ' + voices.length + ".  The rate of speech is " + msg.rate;
+    msg.lang = 'en-US';
 
-msg.onerror = function(e) {
-	alert("An error occurred.");
-}
+    msg.onend = function(e) {
+      console.log('Finished in ' + event.elapsedTime + ' seconds.');
+    };
 
+    msg.onerror = function(e) {
+    	console.error("An error occurred.");
+    }
 
-speechSynthesis.speak(msg);
+    speechSynthesis.speak(msg);
+  };
+
+})();
