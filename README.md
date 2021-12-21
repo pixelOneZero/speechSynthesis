8cfc8062e45ef34d6b3bde671b8ea2a8dc98f2ee
Michael McCarthy <mike@pixelonezero.com>
2021-12-20 20:18:10 -0800

    *Add form fields with identifiers to html * Read text entered into textarea in the initialization function in speech.js * Update README file with instructions for running the demo locally

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 speechSynthesis
 ===============
+
+To run this demo locally, enter the following at the command line:
+python -m SimpleHTTPServer
+
+(Assumes you have MacOS and are running Python)
+
+Then view the demo at http://0.0.0.0:8000/ in a browser.
--- js/speech.js
+++ js/speech.js
@@ -1,15 +1,17 @@
 (() => {
 
   window.initialize = () => {
+    const message = document.querySelector('#message');
     const msg = new SpeechSynthesisUtterance();
     var voices = window.speechSynthesis.getVoices();
+
     msg.voice = voices[0]; // Note: some voices don't support altering params
     msg.voiceURI = 'native';
     msg.volume = 1; // 0 to 1
     msg.rate = 1; // 0.1 to 10
-    msg.pitch = 1; //0 to 2
-    msg.text = 'This is the speech synthesis API. The length of voices is ' + voices.length + ".  The rate of speech is " + msg.rate;
-    msg.lang = 'en-US';
+    msg.pitch = .9; //0 to 2
+    msg.text = message?.value?.length > 0 ? message.value : 'This is the speech synthesis API. The length of voices is ' + voices.length + ".  The rate of speech is " + msg.rate;
+    msg.lang = 'fr-ca';
 
     msg.onend = function(e) {
       console.log('Finished in ' + event.elapsedTime + ' seconds.');
