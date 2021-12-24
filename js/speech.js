d61b1fb8a4e3f6f687f74d44ac4f15f7ad7680ca
Michael McCarthy <mike@pixelonezero.com>
2021-12-23 21:18:34 -0800

    Add speech namespace to speech.js. Add pause button and stub pause function.

--- js/speech.js
+++ js/speech.js
@@ -1,27 +1,33 @@
 (() => {
 
-  window.initialize = () => {
-    const message = document.querySelector('#message');
-    const msg = new SpeechSynthesisUtterance();
-    var voices = window.speechSynthesis.getVoices();
+  window.speech = {
+    initialize: () => {
+      const message = document.querySelector('#message');
+      const msg = new SpeechSynthesisUtterance();
+      var voices = window.speechSynthesis.getVoices();
 
-    msg.voice = voices[0]; // Note: some voices don't support altering params
-    msg.voiceURI = 'native';
-    msg.volume = 1; // 0 to 1
-    msg.rate = 1; // 0.1 to 10
-    msg.pitch = .9; //0 to 2
-    msg.text = message?.value?.length > 0 ? message.value : 'This is the speech synthesis API. The length of voices is ' + voices.length + ".  The rate of speech is " + msg.rate;
-    msg.lang = 'en-US';
+      msg.voice = voices[0]; // Note: some voices don't support altering params
+      msg.voiceURI = 'native';
+      msg.volume = 1; // 0 to 1
+      msg.rate = 1; // 0.1 to 10
+      msg.pitch = .9; //0 to 2
+      msg.text = message?.value?.length > 0 ? message.value : 'This is the speech synthesis API. The length of voices is ' + voices.length + ".  The rate of speech is " + msg.rate;
+      msg.lang = 'en-US';
 
-    msg.onend = function(e) {
-      console.log('Finished in ' + event.elapsedTime + ' seconds.');
-    };
+      msg.onend = function(e) {
+        console.log('Finished in ' + event.elapsedTime + ' seconds.');
+      };
 
-    msg.onerror = function(e) {
-    	console.error("An error occurred.");
+      msg.onerror = function(e) {
+      	console.error("An error occurred.");
+      }
+
+      speechSynthesis.speak(msg);
+    },
+
+    pause: () => {
+      console.log('pause invoked');
     }
-
-    speechSynthesis.speak(msg);
-  };
+  }
 
 })();
