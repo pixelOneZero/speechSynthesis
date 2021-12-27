e0a0fe0d91ce94f82aec0980a0535022273a7fea
Michael McCarthy <mike@pixelonezero.com>
2021-12-26 20:51:54 -0800

    Update variable name to be more descriptive.

--- js/speech.js
+++ js/speech.js
@@ -3,26 +3,26 @@
   window.speech = {
     initialize: () => {
       const message = document.querySelector('#message');
-      const msg = new SpeechSynthesisUtterance();
+      const speech = new SpeechSynthesisUtterance();
       var voices = window.speechSynthesis.getVoices();
 
-      msg.voice = voices[0]; // Note: some voices don't support altering params
-      msg.voiceURI = 'native';
-      msg.volume = 1; // 0 to 1
-      msg.rate = 1; // 0.1 to 10
-      msg.pitch = .9; //0 to 2
-      msg.text = message?.value?.length > 0 ? message.value : 'This is the speech synthesis API. The length of voices is ' + voices.length + ".  The rate of speech is " + msg.rate;
-      msg.lang = 'en-US';
+      speech.voice = voices[0]; // Note: some voices don't support altering params
+      speech.voiceURI = 'native';
+      speech.volume = 1; // 0 to 1
+      speech.rate = 1; // 0.1 to 10
+      speech.pitch = .9; //0 to 2
+      speech.text = message?.value?.length > 0 ? message.value : 'This is the speech synthesis API. The length of voices is ' + voices.length + ".  The rate of speech is " + speech.rate;
+      speech.lang = 'en-US';
 
-      msg.onend = function(e) {
+      speech.onend = function(e) {
         console.log('Finished in ' + event.elapsedTime + ' seconds.');
       };
 
-      msg.onerror = function(e) {
+      speech.onerror = function(e) {
       	console.error("An error occurred.");
       }
 
-      speechSynthesis.speak(msg);
+      speechSynthesis.speak(speech);
     },
 
     pause: () => {
